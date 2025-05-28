cdb32f622203822a4392f480c3c0258af3600fed
kauan <kauancavalcante8p@gmail.com>
2025-05-28 09:08:19 -0300

    classe abstrata

---
+++ POJO/Abelha.java
@@ -0,0 +1,13 @@
+package POJO;
+
+public abstract class Abelha {
+    private String nomeEspecie;
+    private int qtdMelPorHora;
+
+    public Abelha(String nome, int qtdMelPorHora) {
+        this.nomeEspecie = nome;
+        this.qtdMelPorHora = qtdMelPorHora;
+    }
+
+
+}
