8f1fcecae2abf292d7af2b9d58a7dc938e3dd1bc
kauan <kauancavalcante8p@gmail.com>
2025-05-28 09:32:04 -0300

    classes de abelhas

--- POJO/Abelha.java
+++ POJO/Abelha.java
@@ -1,13 +1,30 @@
 package POJO;
 
-public abstract class Abelha {
-    private String nomeEspecie;
-    private int qtdMelPorHora;
+import java.time.LocalTime;
 
-    public Abelha(String nome, int qtdMelPorHora) {
-        this.nomeEspecie = nome;
-        this.qtdMelPorHora = qtdMelPorHora;
+public abstract class Abelha {
+    private String categoria;
+    private int qtdMelPorSeg;
+    private LocalTime entrouNaComeia;
+
+    public Abelha(String categoria, int qtdMelPorSeg) {
+        this.categoria = categoria;
+        this.qtdMelPorSeg = qtdMelPorSeg;
     }
 
+    public String getCategoria() {
+        return categoria;
+    }
 
+    public int getQtdMelPorSeg() {
+        return qtdMelPorSeg;
+    }
+
+    public LocalTime getEntrouNaComeia() {
+        return entrouNaComeia;
+    }
+
+    public void setEntrouNaComeia(LocalTime entrouNaComeia) {
+        this.entrouNaComeia = entrouNaComeia;
+    }
 }
